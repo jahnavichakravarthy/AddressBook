9c2c4311edb94810a11cd8d8584427c6180eb073
jahnavichakravarthy <59594409+jahnavichakravarthy@users.noreply.github.com>
2020-05-06 10:32:33 +0530

    work by 06/05/2020

--- AddressBook/AddressBook.js
+++ AddressBook/AddressBook.js
@@ -2,31 +2,41 @@
 //remove
 //
 //   function Delete() {
-  $(document).ready(function() {
-    $(window).unload(saveSettings);
-    loadSettings();//Name,Id,Address,PhoneNumber
+ // $(document).ready(function() {}
+  //   $(window).unload(saveSettings);
+  //   loadSettings();//Name,Id,Address,PhoneNumber
 // });
 //   }
 ///forms for detilas to be done
-function loadSettings() {
-  $('#Name').val(localStorage.Name);
-  $('#Id').val(localStorage.Id);
-  $('#Address').val(localStorage.Address);
+var name=document.getElementById('txt_name');
+var id=document.getElementById('txt_id');
+var address=document.getElementById('txt_address');
+var phonenumber=document.getElementById('txt_phonenumber');
+function AddContact() 
+{
   
-  $("#PhoneNumber").val(localStorage.PhoneNumber);
-  saveSettings();
-}
+var Contact={Name:name.value,Id :id.value,Address:address.value,PhoneNumber:phonenumber.value,};
+localStorage.setItem('Contact', JSON.stringify(Contact));}
+// function loadSettings() {
+//   $('#Name').val(localStorage.Name);
+//   $('#Id').val(localStorage.Id);
+//   $('#Address').val(localStorage.Address);
+  
+//   $("#PhoneNumber").val(localStorage.PhoneNumber);
+//   saveSettings();
+// }
 
-function saveSettings() {
-  localStorage.Name = $('#Name').val();
-  localStorage.Id = $('#Id').val();
-  localStorage.Address = $('#Address').val();
-  localStorage.PhoneNumber = $("#PhoneNumber").val();
+// function saveSettings() {
+//   localStorage.Name = $('#Name').val();
+//   localStorage.Id = $('#Id').val();
+//   localStorage.Address = $('#Address').val();
+//   localStorage.PhoneNumber = $("#PhoneNumber").val();
   
-}
+// }
+  
 
 // function AddContact(name, email, mobile, landline, website, address) 
-//   {
+//   {var inputEmail= document.getElementById("email");
 //             this.name = name;
 //             this.email = email;
 //             this.mobile = mobile;
@@ -63,7 +73,3 @@ function saveSettings() {
 //             //     });
 //             //   });
 //         }
-
-
-
-  
